8cbba805eb077fc7db04ead5917c9fc430fbf446
danepozzi <danepozzi@gmail.com>
2024-05-20 16:56:00 +0200

    add proxy to deploy

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ echo "copying index.html and build/elm.js to /var/www/html/ and a json"
 cp index.html /var/www/html/index.html
 cp build/elm.js /var/www/html/build/elm.js
 cp kcpedia.json /var/www/html/kcpedia.json
+cp proxy.go ../rcproxy/proxy.go
