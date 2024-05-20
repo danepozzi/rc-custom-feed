e7fa5f1cec285a4f7dd719d0435e46c992318f16
danepozzi <danepozzi@gmail.com>
2024-05-20 11:56:59 +0200

    invert key value to avoid unnecessary conversion

--- all-portals.py
+++ all-portals.py
@@ -11,12 +11,12 @@ for entry in data:
     if 'published_in' in entry:
         for item in entry['published_in']:
             if 'name' in item:
-                portals[item['id']] = item['name']
+                portals[item['name']] = item['id']
 
     if 'connected_to' in entry:
         for item in entry['connected_to']:
             if 'name' in item:
-                portals[item['id']] = item['name']
+                portals[item['name']] = item['id']
 
 with open('all_portals.json', 'w') as outfile:
     json.dump(portals, outfile)
