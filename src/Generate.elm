fe5eb5e477ae9eeaa618cdc6e57b56fff63968a6
danepozzi <danepozzi@gmail.com>
2024-05-20 13:50:52 +0200

    portal select

--- src/Generate.elm
+++ src/Generate.elm
@@ -1,21 +1,48 @@
 module Generate exposing (main)
 
 import Browser exposing (..)
+import Dict exposing (Dict)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Json.Decode exposing (Decoder, decodeString, dict, int)
+import Portals exposing (portalsList)
+
+
+type alias PortalDict =
+    Dict String Int
+
+
+portalDecoder : Decoder PortalDict
+portalDecoder =
+    dict int
 
 
 type alias Model =
-    { keyword : String
+    { portal : Maybe Int
+    , keyword : String
     , elements : Int
     , order : String
+    , portals : PortalDict
+    , error : Maybe String
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { keyword = "", elements = 2, order = "recent" }, Cmd.none )
+    let
+        jsonString =
+            portalsList
+
+        decodedPortals =
+            decodeString portalDecoder jsonString
+    in
+    case decodedPortals of
+        Ok portals ->
+            ( { portal = Nothing, keyword = "", elements = 2, order = "recent", portals = portals, error = Nothing }, Cmd.none )
+
+        Err error ->
+            ( { portal = Nothing, keyword = "", elements = 2, order = "recent", portals = Dict.empty, error = Just (errorToString error) }, Cmd.none )
 
 
 type Msg
@@ -23,8 +50,11 @@ type Msg
     | Decrement
     | UpdateKeyword String
     | SetOrder String
+    | SetPortal String
+    | FetchData
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
@@ -39,12 +69,37 @@ update msg model =
         SetOrder newOrder ->
             ( { model | order = newOrder }, Cmd.none )
 
+        SetPortal portalName ->
+            ( let
+                newPortal =
+                    getPortalId model.portals portalName
+              in
+              { model
+                | portal = newPortal
+              }
+            , Cmd.none
+            )
+
+        FetchData ->
+            ( model, Cmd.none )
+
+
+errorToString : Json.Decode.Error -> String
+errorToString error =
+    Json.Decode.errorToString error
+
 
 view : Model -> Html Msg
 view model =
     let
+        portalId =
+            model.portal
+
+        portalAsString =
+            portalIdToString portalId
+
         url =
-            "https://rcfeed.sarconference2016.net/?keyword=" ++ model.keyword ++ "&elements=" ++ String.fromInt model.elements ++ "&order=" ++ model.order ++ "\""
+            "https://rcfeed.sarconference2016.net/?keyword=" ++ model.keyword ++ "&elements=" ++ String.fromInt model.elements ++ "&order=" ++ model.order ++ "&portal=" ++ portalAsString ++ "\""
 
         iframeHeight =
             if model.elements < 4 then
@@ -53,7 +108,8 @@ view model =
             else
                 500
 
-        --round (1000 / toFloat model.elements)
+        portalOptions =
+            List.map portalOption (Dict.keys model.portals)
     in
     div [ align "center" ]
         [ div []
@@ -61,23 +117,23 @@ view model =
                 [ text "Generate Feed" ]
             ]
         , div []
+            [ text "Portal: "
+            , select [ onInput SetPortal ]
+                portalOptions
+            ]
+        , div []
             [ text "Keyword: "
             , input [ placeholder "Type your keyword here", value model.keyword, onInput UpdateKeyword ] []
-            ]
-        , div
-            []
-            [ text "Number of Elements to Display: "
+            , text "Number of Elements to Display: "
             , button [ onClick Decrement ] [ text "-" ]
             , text (String.fromInt model.elements)
             , button [ onClick Increment ] [ text "+" ]
-            ]
-        , div []
-            [ text "Order of Elements: "
-            , select [ width 300, onInput SetOrder ]
+            , text "Order of Elements: "
+            , select [ onInput SetOrder ]
                 (List.map orderOption [ "recent", "random" ])
             ]
-        , div
-            []
+        , br [] []
+        , div []
             [ iframe
                 [ width 1200, height iframeHeight, src url ]
                 []
@@ -89,8 +145,29 @@ view model =
         ]
 
 
+portalOption : String -> Html msg
+portalOption portalName =
+    option [ value portalName ] [ text portalName ]
+
+
+getPortalId : PortalDict -> String -> Maybe Int
+getPortalId portals portalName =
+    Dict.get portalName portals
+
+
+orderOption : String -> Html msg
 orderOption order =
     option [ value order ] [ text order ]
+
+
+portalIdToString : Maybe Int -> String
+portalIdToString id =
+    case id of
+        Just int ->
+            String.fromInt int
+
+        Nothing ->
+            ""
 
 
 main : Program () Model Msg
