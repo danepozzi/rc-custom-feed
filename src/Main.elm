7e70d90b6dba49519260d694b8c0a3803230a781
danepozzi <danepozzi@gmail.com>
2024-06-16 11:45:21 +0100

    ellipsis

--- src/Main.elm
+++ src/Main.elm
@@ -806,7 +806,7 @@ viewTitleAuthorAbstract w columns exp =
                     title + author
 
                 shortAbstract =
-                    String.Extra.softEllipsis (round (toFloat w / (toFloat columns * 1.5)) - chars) exposition.abstract
+                    String.Extra.softEllipsis (round (toFloat w / (toFloat columns * 1.6)) - chars) exposition.abstract
 
                 cols =
                     if w > 675 then
