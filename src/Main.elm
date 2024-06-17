1a575f0d23f903de40ec33881037e4c57a5f89fa
danepozzi <danepozzi@gmail.com>
2024-06-17 07:57:50 +0100

    column view

--- src/Main.elm
+++ src/Main.elm
@@ -24,7 +24,7 @@ import Url exposing (Url)
 
 
 columnRatios =
-    Array.fromList [ 7 / 12, 7 / 12, 7 / 13, 7 / 17, 7 / 21, 7 / 25 ]
+    Array.fromList [ 7 / 12, 7 / 8, 7 / 12, 7 / 16, 7 / 20, 7 / 24 ]
 
 
 wideRatios =
